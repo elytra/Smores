919124ce359d94f25039c9e822859a1561236a39
Ethan Brooks <EB5473@gmail.com>
2017-05-06 01:29:33 -0400

    Reworked recipe registry to not give incorrect items

--- src/main/java/com/elytradev/smores/registries/RecipeRegistry.java
+++ src/main/java/com/elytradev/smores/registries/RecipeRegistry.java
@@ -27,11 +27,14 @@
 
 package com.elytradev.smores.registries;
 
+import com.elytradev.smores.Smores;
 import com.elytradev.smores.materials.EnumAlloy;
 import com.elytradev.smores.materials.EnumGem;
 import com.elytradev.smores.materials.EnumMetal;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
 
@@ -39,31 +42,62 @@ public class RecipeRegistry {
     public static void init() {
         // TODO: Disable the shit out of these.
         for(EnumMetal metal : EnumMetal.values()) {
-            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ItemRegistry.itemIngot, 1, metal.getId()), new Object[]{"NNN", "NNN", "NNN", 'N', "nugget"+ metal.getMaterialName()}));
-            GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(ItemRegistry.itemNugget, 9, metal.getId()), new Object[]{"ingot"+ metal.getMaterialName()}));
-            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ItemRegistry.itemGear, 1, metal.getId()), new Object[]{"*M*","MIM","*M*",'M',"ingot"+metal.getMaterialName(),'I',"ingotIron"}));
-            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockRegistry.blockMetal, 1, metal.getId()), new Object[]{"III", "III", "III", 'I', "ingot"+ metal.getMaterialName()}));
-            GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(ItemRegistry.itemIngot, 9, metal.getId()), new Object[]{"block"+ metal.getMaterialName()}));
-
-            GameRegistry.addSmelting(new ItemStack(BlockRegistry.blockMetalOre, 1, metal.getId()), new ItemStack(ItemRegistry.itemIngot, 1, metal.getId()), 0.7f);
+            constructShapedOreRecipe("ingot"+metal.getMaterialName(),1, new Object[]{"NNN", "NNN", "NNN", 'N', "nugget" + metal.getMaterialName()});
+            constructShapelessOreRecipe("nugget"+metal.getMaterialName(), 9, new Object[]{"ingot" + metal.getMaterialName()});
+            constructShapedOreRecipe("gear"+metal.getMaterialName(), 1, new Object[]{"*M*", "MIM", "*M*", 'M', "ingot" + metal.getMaterialName(), 'I', "ingotIron"});
+            constructShapedOreRecipe("block"+metal.getMaterialName(), 1, new Object[]{"III", "III", "III", 'I', "ingot"+ metal.getMaterialName()});
+            constructShapelessOreRecipe("ingot"+metal.getMaterialName(), 9, new Object[]{"block"+ metal.getMaterialName()});
+            constructSmeltingRecipe("ore"+metal.getMaterialName(),"ingot"+metal.getMaterialName(),0.7f);
         }
 
         for(EnumAlloy alloy : EnumAlloy.values()) {
-            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ItemRegistry.itemIngot, 1, alloy.getId()), new Object[]{"NNN", "NNN", "NNN", 'N', "nugget"+ alloy.getMaterialName()}));
-            GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(ItemRegistry.itemNugget, 9, alloy.getId()), new Object[]{"ingot"+ alloy.getMaterialName()}));
-            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ItemRegistry.itemGear, 1, alloy.getId()), new Object[]{"*M*","MIM","*M*",'M',"ingot"+alloy.getMaterialName(),'I',"ingotIron"}));
-            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockRegistry.blockAlloy, 1, alloy.getId()), new Object[]{"III", "III", "III", 'I', "ingot"+ alloy.getMaterialName()}));
-            GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(ItemRegistry.itemIngot, 9, alloy.getId()), new Object[]{"block"+ alloy.getMaterialName()}));
+            constructShapedOreRecipe("ingot"+alloy.getMaterialName(),1, new Object[]{"NNN", "NNN", "NNN", 'N', "nugget" + alloy.getMaterialName()});
+            constructShapelessOreRecipe("nugget"+alloy.getMaterialName(), 9, new Object[]{"ingot" + alloy.getMaterialName()});
+            constructShapedOreRecipe("gear"+alloy.getMaterialName(), 1, new Object[]{"*M*", "MIM", "*M*", 'M', "ingot" + alloy.getMaterialName(), 'I', "ingotIron"});
+            constructShapedOreRecipe("block"+alloy.getMaterialName(), 1, new Object[]{"III", "III", "III", 'I', "ingot"+ alloy.getMaterialName()});
+            constructShapelessOreRecipe("ingot"+alloy.getMaterialName(), 9, new Object[]{"block"+ alloy.getMaterialName()});
+            constructSmeltingRecipe("ore"+alloy.getMaterialName(),"ingot"+alloy.getMaterialName(),0.7f);
         }
 
         for(EnumGem gem : EnumGem.values()) {
-            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockRegistry.blockGem, 1, gem.getId()), new Object[]{"GGG", "GGG", "GGG", 'G', "gem"+ gem.getMaterialName()}));
-            GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(ItemRegistry.itemGem, 9, gem.getId()), new Object[]{"block"+ gem.getMaterialName()}));
-
-            GameRegistry.addSmelting(new ItemStack(BlockRegistry.blockGemOre, 1, gem.getId()), new ItemStack(ItemRegistry.itemGem, 1, gem.getId()), 1.0f);
-
+            constructShapedOreRecipe("block"+gem.getMaterialName(), 1,new Object[]{"GGG", "GGG", "GGG", 'G', "gem" + gem.getMaterialName()});
+            constructShapelessOreRecipe("gem"+gem.getMaterialName(), 9, new Object[]{"block"+ gem.getMaterialName()});
+            constructSmeltingRecipe("ore"+gem.getMaterialName(), "gem"+gem.getMaterialName(), 1.0f);
         }
+    }
 
+    private static void constructShapedOreRecipe(String product, int count, Object[] recipe) {
+        NonNullList<ItemStack> products = OreDictionary.getOres(product);
+        if(products.size() <= 0) {
+            Smores.LOG.error("Couldn't get an OreDict reference for " + product + " - failed to register recipe!");
+        }
+        else {
+            ItemStack productStack = products.get(0).copy();
+            productStack.setCount(count);
+            GameRegistry.addRecipe(new ShapedOreRecipe(productStack, recipe));
+        }
+    }
 
+    private static void constructShapelessOreRecipe(String product, int count, Object[] recipe) {
+        NonNullList<ItemStack> products = OreDictionary.getOres(product);
+        if(products.size() <= 0) {
+            Smores.LOG.error("Couldn't get an OreDict reference for " + product + " - failed to register recipe!");
+        }
+        else {
+            ItemStack productStack = products.get(0).copy();
+            productStack.setCount(count);
+            GameRegistry.addRecipe(new ShapelessOreRecipe(productStack, recipe));
+        }
+    }
+
+    private static void constructSmeltingRecipe(String supply, String product, float exp) {
+        NonNullList<ItemStack> supplies = OreDictionary.getOres(supply);
+        NonNullList<ItemStack> products = OreDictionary.getOres(product);
+
+        if(supplies.size() != 0 && products.size() != 0) {
+            ItemStack supplyStack = supplies.get(0).copy();
+            ItemStack productStack = products.get(0).copy();
+            GameRegistry.addSmelting(supplyStack, productStack , exp);
+        }
     }
 }
