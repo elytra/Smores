64a87563c248410a435ad26dcfda6450f70aa65b
Una Thompson <una@unascribed.com>
2018-01-01 01:12:07 -0500

    Skeletize, update LICENSE

--- src/main/java/com/elytradev/smores/block/BlockSubtyped.java
+++ src/main/java/com/elytradev/smores/block/BlockSubtyped.java
@@ -1,9 +1,10 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2017:
+ * Copyright (c) 2017-2018
  *     Ethan Brooks (CalmBit),
  *     Isaac Ellingson (Falkreon),
+ *     Una Thompson (unascribed),
  *     and contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
